a7ad2e5979404ae6d5df1d341f40d4e6287e06ee
victorcantino <victor.cantino@gmail.com>
2022-08-30 20:13:17 -0300

    Laravel Mix

--- resources/views/series/create.blade.php
+++ resources/views/series/create.blade.php
@@ -1,6 +1,10 @@
 <x-layout title="Nova série">
     <form action="" method="post">
-        <label for="nome">Nome</label>
-        <input type="text" name="nome" id="nome">
+        <div class="mb-3">
+            <label for="nome" class="form-label">Nome</label>
+            <input type="text" name="nome" id="nome" class="form-">
+        </div>
+
+        <input type="submit" value="Adicionar" class="btn btn-primary">
     </form>
 </x-layout>
