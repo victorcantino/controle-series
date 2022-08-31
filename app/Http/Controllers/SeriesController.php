8c2430561fefcdd12c6df79b3c4c28bddc04a9d7
victorcantino <victor.cantino@gmail.com>
2022-08-31 00:40:13 -0300

    Model

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -2,25 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Serie;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class SeriesController extends Controller
 {
-    public function index(Request $request) 
+    public function index() 
     {
-        //dd($request);
-
-        $series = [
-            'série 1',
-            'série 2',
-            'série 3',
-        ];
-
-        // return view('listar-series', compact('series'));
+        // $series = Serie::all();
+        $series = Serie::query()->orderBy('nome')->get();
         return view('series.index')->with('series', $series);
+        // return view('listar-series', compact('series'));
     }
 
     public function create() {
         return view('series.create');
+    }
+
+    public function store(Request $request)
+    {
+        $serie = new Serie();
+        $serie->nome = $request->input('nome');
+        $serie->save();
+        return redirect('/series');
     }
 }
